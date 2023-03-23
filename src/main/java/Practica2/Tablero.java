3d4969cfd992e5e6bd49fe3dd214271a1d55ba1b
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-03-23 19:49:37 +0100

    Creacion clases practica 2

---
+++ src/main/java/Practica2/Tablero.java
@@ -0,0 +1,10 @@
+package Practica2;
+
+public class Tablero {
+    private static int DIMENSION = 30;
+    private int[][] estadoActual;
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
+
+
+
+}
