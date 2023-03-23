a1d54adcf722262a1c40719a3682f375cbce3002
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-03-23 19:44:14 +0100

    clase aplicacion

--- src/main/java/Practica1/Aplicacion.java
+++ src/main/java/Practica1/Aplicacion.java
@@ -1,4 +1,7 @@
 package Practica1;
 
 public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("El número PI es " + Matematicas.generarNumeroPi((long)(Math.random() * 200) + 1));
+    }
 }
