14f93fd62033e81e52256b8678c15074a42e335f
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-03-23 19:30:30 +0100

    clase matematicas

--- src/main/java/Practica1/Matematicas.java
+++ src/main/java/Practica1/Matematicas.java
@@ -1,4 +1,16 @@
 package Practica1;
 
 public class Matematicas {
+    public static double generarNumeroPi(long pasos){
+        int aciertos = 0;
+        double areaCuadrado = 4;
+        for (int i = 1; i <= pasos; i++){
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            if (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) <= 1){
+                aciertos++;
+            }
+        }
+        return (areaCuadrado * (aciertos / (double) pasos));
+    }
 }
